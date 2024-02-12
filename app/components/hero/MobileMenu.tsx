7330cac5e9e249babae885c12b808951b9cf6f9a
giorgio <giorgiosavron@me.com>
2024-02-12 15:24:19 -0500

    mobile menu open scrolling

--- app/components/hero/MobileMenu.tsx
+++ app/components/hero/MobileMenu.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect } from "react";
 import Link from "next/link";
 import { links, resumeLink } from "../../assets/links";
@@ -13,11 +14,18 @@ export const MobileMenu: React.FC<MobileMenuProps> = (props) => {
 
   //do not allow scrolling when mobile menu is open
   useEffect(() => {
-    if (open === "open") {
-      document.body.style.overflow = "hidden";
-    } else if (open === "closed") {
-      document.body.style.overflow = "unset";
-    }
+    // Function to handle body overflow
+    const handleBodyOverflow = () => {
+      document.body.style.overflow = open ? "hidden" : "auto";
+    };
+
+    // Set body overflow when modal is open
+    handleBodyOverflow();
+
+    // Cleanup function to restore body overflow when component unmounts or modal closes
+    return () => {
+      document.body.style.overflow = "auto";
+    };
   }, [open]);
 
   //map out links form links var
