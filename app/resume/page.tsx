fc84c026f74b5bbcbb97db9a9e4a5ae5a56b1886
giorgio <giorgiosavron@me.com>
2024-03-06 13:21:57 -0500

    resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,7 +2,7 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import { NavBar } from "../layout/nav-bar";
-import resume from "https://giorgiosavrondotcom.s3.amazonaws.com/gsxresume.pdf";
+import resume from "https://giorgiosavrondotcom.s3.amazonaws.com/gxresume.pdf";
 
 export const revalidate = 3600;
 
