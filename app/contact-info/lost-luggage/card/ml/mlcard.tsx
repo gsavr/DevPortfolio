ab1c2da37bb2b3ed6587b89bcf96605ac383b5e0
giorgio <giorgiosavron@me.com>
2026-04-09 11:46:46 -0400

    contact if found

--- app/contact-info/lost-luggage/card/ml/mlcard.tsx
+++ app/contact-info/lost-luggage/card/ml/mlcard.tsx
@@ -4,51 +4,52 @@ import { img } from "../../../../assets/contactCard-img";
 
 export const MLCard: React.FC = () => {
   return (
-    <div className="container m-3 flex w-96 flex-col items-center rounded-xl p-4 shadow-xl">
-      <div className="h-72">
-        <Image
-          width={300}
-          height={300}
-          className="rounded-xl shadow-md"
-          src={img.matthew}
-          alt={"profile-picture"}
-        />
-      </div>
-      <div className="mt-2 text-center">
-        <div color="blue-gray" className="mb-2 mt-4 text-xl font-semibold">
-          Matthew Land
+    <div className="flex h-screen w-screen justify-center bg-slate-100">
+      <div className="container m-3 flex h-fit w-96 flex-col items-center rounded-xl bg-white p-4 shadow-xl">
+        <div>
+          <Image
+            width={175}
+            height={175}
+            className="rounded-full shadow-lg"
+            src={img.matthew}
+            alt={"profile-picture"}
+          />
         </div>
-
-        <div
-          color="blue-gray"
-          className="flex flex-col items-center justify-center font-medium"
-        >
-          <div className="flex items-center">
-            <Image
-              width={20}
-              height={20}
-              src={img.phone}
-              alt={"phone"}
-              className="mr-2"
-            />
-            <a href="tel:+18504453428">+1 (850) 445-3428</a> /{" "}
+        <div className="text-center">
+          <div color="blue-gray" className="mb-2 mt-4 text-xl font-semibold">
+            Matthew Land
           </div>
-          <div className="flex items-center">
-            <Image
-              width={20}
-              height={20}
-              src={img.email}
-              alt={"email"}
-              className="mr-2"
-            />
-            <a href="mailto:matthewland@me.com">matthewland@me.com</a>
+          <div
+            color="blue-gray"
+            className="flex flex-col items-center justify-center font-medium"
+          >
+            <div className="flex items-center">
+              <Image
+                width={20}
+                height={20}
+                src={img.phone}
+                alt={"phone"}
+                className="mr-2"
+              />
+              <a href="tel:+18504453428">+1 (850) 445-3428</a> /{" "}
+            </div>
+            <div className="flex items-center">
+              <Image
+                width={20}
+                height={20}
+                src={img.email}
+                alt={"email"}
+                className="mr-2"
+              />
+              <a href="mailto:matthewland@me.com">matthewland@me.com</a>
+            </div>
           </div>
+          <div color="blue-gray" className="m-2 font-medium">
+            <p>1724 Connecticut Ave NW Apt 2</p>
+            <p>Washington, DC 20009</p>
+          </div>
+          <div className="text-sm">If found, please contact me.</div>
         </div>
-        <div color="blue-gray" className="m-2 font-medium">
-          <p>1724 Connecticut Ave NW Apt 2</p>
-          <p>Washington, DC 20009</p>
-        </div>
-        <div className="text-sm">If found, please contact me.</div>
       </div>
     </div>
   );
